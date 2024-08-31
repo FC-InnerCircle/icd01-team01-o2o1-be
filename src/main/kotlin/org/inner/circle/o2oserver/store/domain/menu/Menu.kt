2025727eac93daf9657004b80bbb53790b725397
devchanki <kgichan12@gmail.com>
2024-08-31 17:33:25 +0900

    코드 병합을 위해 임시로 푸시(음식점 메뉴 기능 추후 구현)

--- src/main/kotlin/org/inner/circle/o2oserver/store/domain/menu/Menu.kt
+++ src/main/kotlin/org/inner/circle/o2oserver/store/domain/menu/Menu.kt
@@ -1,3 +1,9 @@
 package org.inner.circle.o2oserver.store.domain.menu
 
-class Menu
+class Menu (
+    val menuId: Int?,
+    val menuName: String,
+    val description: String?,
+    val menuPrice: Int?,
+    val optionGroups: List<MenuOptionGroup>?
+)
---
+++ src/main/kotlin/org/inner/circle/o2oserver/store/domain/menu/MenuOptionGroup.kt
@@ -0,0 +1,9 @@
+package org.inner.circle.o2oserver.store.domain.menu
+
+class MenuOptionGroup (
+    optionGroupId: Int?,
+    optionGroupName: String?,
+    isRequired: Boolean?,
+    Options: List<MenuOption>?,
+) {
+}
