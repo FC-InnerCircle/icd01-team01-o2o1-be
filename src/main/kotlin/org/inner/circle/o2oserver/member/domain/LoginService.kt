dcbfe5d2399cf681b4c35058a83e2641d89bf054
Donghyeon0908 <donghyeonlee0908@gmail.com>
2024-09-07 12:46:32 +0900

    refactor: loginService

--- src/main/kotlin/org/inner/circle/o2oserver/member/domain/LoginService.kt
+++ src/main/kotlin/org/inner/circle/o2oserver/member/domain/LoginService.kt
@@ -15,31 +15,42 @@ class LoginService(
 
     @Transactional
     override fun findOrCreateMember(member: Member): MemberCreationResult {
-        val existingMember = memberReader.findBySnsTypeAndSubId(member.snsType, member.subId)
+        val existingMember = findExistingMember(member)
 
-        return if (existingMember != null) {
-            MemberCreationResult(existingMember, false)
-        } else {
-            val memberId = sequenceGenerator.generate("memberSequence")
-
-            val memberInfo = Member(
-                memberId = memberId,
-                name = member.name,
-                snsType = member.snsType,
-                subId = member.subId,
-            )
-            val newMember = memberStore.save(memberInfo)
-            log.info("New member created with ID: ${newMember.id}")
-
-            try {
-                memberOutPort.sendMemberData(newMember)
-            } catch (e: Exception) {
-                throw RuntimeException(
-                    "External server communication failed, rolling back transaction",
-                    e,
-                )
-            }
+        return existingMember?.let {
+            MemberCreationResult(it, false)
+        } ?: run {
+            val newMember = createNewMember(member)
+            sendMemberDataToExternalServer(newMember)
             MemberCreationResult(newMember, true)
+        }
+    }
+
+    private fun findExistingMember(member: Member): Member? {
+        return memberReader.findBySnsTypeAndSubId(member.snsType, member.subId)
+    }
+
+    private fun createNewMember(member: Member): Member {
+        val memberId = sequenceGenerator.generate("memberSequence")
+        val memberInfo = Member(
+            memberId = memberId,
+            name = member.name,
+            snsType = member.snsType,
+            subId = member.subId,
+        )
+        return memberStore.save(memberInfo).also {
+            log.info("New member created with ID: ${it.id}")
+        }
+    }
+
+    private fun sendMemberDataToExternalServer(member: Member) {
+        try {
+            memberOutPort.sendMemberData(member)
+        } catch (e: Exception) {
+            throw RuntimeException(
+                "External server communication failed, rolling back transaction",
+                e,
+            )
         }
     }
 }
