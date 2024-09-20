064366cfce35b11b0149bf070426a1ea4385dfc2
Mason <donghyeonlee@megazone.com>
2024-09-20 09:52:14 +0900

    fix: 위경도 유효성검사 추가

--- src/main/kotlin/org/inner/circle/o2oserver/member/presentation/dto/AddressRequest.kt
+++ src/main/kotlin/org/inner/circle/o2oserver/member/presentation/dto/AddressRequest.kt
@@ -1,7 +1,8 @@
 package org.inner.circle.o2oserver.member.presentation.dto
 
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 import org.inner.circle.o2oserver.member.domain.Address
 
@@ -9,8 +10,16 @@ class AddressRequest {
     data class CreateAddress(
         @field:NotBlank(message = "Address cannot be blank") val address: String,
         @field:NotBlank(message = "Address detail cannot be blank") val addressDetail: String,
-        @field:NotNull(message = "Latitude cannot be null") val latitude: Double? = null,
-        @field:NotNull(message = "Longitude cannot be null") val longitude: Double? = null,
+        @field:DecimalMin(
+            value = "33.0",
+            inclusive = true,
+            message = "Latitude must be at least 33.0",
+        ) @field:DecimalMax(value = "39.0", inclusive = true, message = "Latitude must be at most 39.0") val latitude: Double,
+        @field:DecimalMin(
+            value = "124.0",
+            inclusive = true,
+            message = "Longitude must be at least 124.0",
+        ) @field:DecimalMax(value = "132.0", inclusive = true, message = "Longitude must be at most 132.0") val longitude: Double,
         @field:NotBlank(message = "Zip code cannot be blank") @field:Pattern(
             regexp = "\\d{5}(?:-\\d{4})?",
             message = "ZipCode must be a valid format (e.g., 12345 or 12345-6789)",
@@ -22,8 +31,8 @@ class AddressRequest {
                     memberId = memberId,
                     address = createAddress.address,
                     addressDetail = createAddress.addressDetail,
-                    latitude = createAddress.latitude!!,
-                    longitude = createAddress.longitude!!,
+                    latitude = createAddress.latitude,
+                    longitude = createAddress.longitude,
                     zipCode = createAddress.zipCode,
                     isDefault = false,
                 )
--- src/main/kotlin/org/inner/circle/o2oserver/member/presentation/dto/MemberRequest.kt
+++ src/main/kotlin/org/inner/circle/o2oserver/member/presentation/dto/MemberRequest.kt
@@ -1,6 +1,8 @@
 package org.inner.circle.o2oserver.member.presentation.dto
 
 import jakarta.validation.Valid
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
@@ -30,7 +32,7 @@ class MemberRequest {
                     memberId = memberId,
                     address = memberInfo.address.address,
                     addressDetail = memberInfo.address.addressDetail,
-                    latitude = memberInfo.address.latitude!!,
+                    latitude = memberInfo.address.latitude,
                     longitude = memberInfo.address.longitude!!,
                     zipCode = memberInfo.address.zipCode,
                     isDefault = true, // 기본 주소로 설정
@@ -41,8 +43,16 @@ class MemberRequest {
         data class AddressRequest(
             @field:NotBlank(message = "Address cannot be blank") val address: String,
             @field:NotBlank(message = "Address detail cannot be blank") val addressDetail: String,
-            @field:NotNull(message = "Latitude cannot be null") val latitude: Double? = null,
-            @field:NotNull(message = "Longitude cannot be null") val longitude: Double? = null,
+            @field:DecimalMin(
+                value = "33.0",
+                inclusive = true,
+                message = "Latitude must be at least 33.0",
+            ) @field:DecimalMax(value = "39.0", inclusive = true, message = "Latitude must be at most 39.0") val latitude: Double,
+            @field:DecimalMin(
+                value = "124.0",
+                inclusive = true,
+                message = "Longitude must be at least 124.0",
+            ) @field:DecimalMax(value = "132.0", inclusive = true, message = "Longitude must be at most 132.0") val longitude: Double,
             @field:NotBlank(message = "Zip code cannot be blank") @field:Pattern(
                 regexp = "\\d{5}(?:-\\d{4})?",
                 message = "ZipCode must be a valid format (e.g., 12345 or 12345-6789)",
